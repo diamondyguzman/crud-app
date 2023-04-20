ae0ce8a43245e9a1233edd433368687b88a57c65
dlllooo <dyguzman@iu.edu>
2023-04-20 16:00:29 -0400

    firestore to fix compat

--- src/useHooks/useFirebase.js
+++ src/useHooks/useFirebase.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-
+import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
